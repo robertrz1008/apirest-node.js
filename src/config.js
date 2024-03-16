ecf9ae23e13198c2dbe2b63a49a382d97acb5c48
Roberto <robert18carl@gmail.com>
2024-03-16 20:34:15 -0300

    postgresql al deploy

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 dotenv.config() 
 
 export const PORT = process.env.PORT || 3000
+export const DB_URL = process.env.DB_URL
 export const DB_LOCALHOST = process.env.DB_LOCALHOST ||"localhost"
 export const DB_PORT = process.env.DB_PORT || 5432
 export const DB_USER = process.env.DB_USER || "postgres"
--- src/db/conectiondb.js
+++ src/db/conectiondb.js
@@ -1,7 +1,9 @@
 import pgsql from "pg"
-import { DB_DATABASE, DB_LOCALHOST, DB_PASSWORD, DB_PORT, DB_USER } from "../config.js"
+import { DB_DATABASE, DB_LOCALHOST, DB_PASSWORD, DB_PORT, DB_URL, DB_USER } from "../config.js"
 
 export const pg =  new pgsql.Pool({
+    connectionString: DB_URL,
+    ssl: true,
     host: DB_LOCALHOST,
     port: DB_PORT,
     user: DB_USER,
